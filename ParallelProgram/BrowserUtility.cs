c1f9bc7cb46779cbf70048323e5ee163a4848db5
amitswaraj61 <aswaraj78@gmail.com>
2020-07-28 17:58:56 +0530

    Added Automation script for parallel execution of test in selenium

---
+++ ParallelProgram/BrowserUtility.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasicProgram.ParallelProgram
+{
+    class BrowserUtility
+    {
+        public IWebDriver Init(IWebDriver driver)
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.facebook.com/";
+            return driver;
+        }
+    }
+}
---
+++ ParallelProgram/ParallelAutomation.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasicProgram.ParallelProgram
+{
+    [TestFixture]
+    [Parallelizable]
+    class ParallelAutomation
+    {
+        readonly IWebDriver driver;
+
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod1()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+
+            IWebElement emailTextFields = Driver.FindElement(By.XPath("//td[1]//input[1]"));
+            emailTextFields.SendKeys("aswaraj78@gmail.com");
+            Thread.Sleep(2000);
+            Driver.Quit();
+        }
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod2()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+
+            IWebElement emailTextFields = Driver.FindElement(By.XPath("//td[1]//input[1]"));
+            emailTextFields.SendKeys("aswaraj78@gmail.com");
+            Thread.Sleep(2000);
+            Driver.Quit();
+        }
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod3()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+
+            IWebElement emailTextFields = Driver.FindElement(By.XPath("//td[1]//input[1]"));
+            emailTextFields.SendKeys("aswaraj78@gmail.com");
+            Thread.Sleep(2000);
+            Driver.Quit();
+
+        }
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod4()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+
+            IWebElement emailTextFields = Driver.FindElement(By.XPath("//td[1]//input[1]"));
+            emailTextFields.SendKeys("aswaraj78@gmail.com");
+            Thread.Sleep(2000);
+            Driver.Quit();
+        }
+        [Test, Category("UAT Testing"), Category("Module1")]
+        public void TestMethod5()
+        {
+            var Driver = new BrowserUtility().Init(driver);
+
+            IWebElement emailTextFields = Driver.FindElement(By.XPath("//td[1]//input[1]"));
+            emailTextFields.SendKeys("aswaraj78@gmail.com");
+            Thread.Sleep(2000);
+            Driver.Quit();
+        }
+    }
+}
+
+
