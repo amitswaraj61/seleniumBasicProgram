0529c74114c1c5f13be13d90eb0b473506820d56
amitswaraj61 <aswaraj78@gmail.com>
2020-07-28 17:59:56 +0530

    Added Automation script to read Pdf in selenium

---
+++ pdf/PdfTestcs.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasicProgram.pdf
+{
+
+    public class PdfTestcs
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void VerifyPDFContent()
+        {
+            try
+            {
+                string source = "http://www.africau.edu/images/default/sample.pdf";
+                driver.Url = source;
+                String pdfContent = Pdfclass.ReadPdfContent(driver.Url);
+
+                Assert.IsTrue(pdfContent.Contains("This is a small demonstration .pdf file"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ pdf/Pdfclass.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumBasicProgram.pdf
+{
+    public class Pdfclass
+    {
+        
+    public static string ReadPdfContent(string appUrl)
+        {
+            StringBuilder test = new StringBuilder(); ////used for read pdf in string
+            using (PdfReader reader = new PdfReader(appUrl))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    test.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+            Console.WriteLine(test.ToString());
+            Console.WriteLine("Total Length" + test.Length.ToString());
+            //     String fileName = "TextFromPDF.txt";
+
+            //     File.WriteAllText(fileName, test.ToString());
+
+            //    System.Diagnostics.Process.Start("TextFromPDF.txt", fileName);
+
+            //     bool result = fileName.Contains("Seeking");
+            //     Console.WriteLine("Result is" + result);
+
+            return test.ToString();
+        }
+    }
+}
+
