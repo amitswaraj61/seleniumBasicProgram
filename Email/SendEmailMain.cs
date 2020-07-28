99c66e95f3f8ff0eb9036a59e156d1307ca9d071
amitswaraj61 <aswaraj78@gmail.com>
2020-07-28 17:54:05 +0530

    Added Automation script for Email functionality in selenium

---
+++ Email/SendEmailMain.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasicProgram.Email
+{
+    class SendEmailMain
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        public void SendMail()
+        {
+            try { 
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://jqueryui.com/resizable/";
+
+            driver.SwitchTo().Frame(0); //move inside the frame ..to perform task ..here only 1 frame
+
+            IWebElement resize = driver.FindElement(By.XPath("//body/div[3]"));
+            Thread.Sleep(2000);
+
+            //Actions class
+            Actions action = new Actions(driver);
+
+            action.MoveToElement(resize).DragAndDropToOffset(resize, 100, 100);
+            //in resizeable first move mouse to element then resize it here we give x and y both to resize
+            Thread.Sleep(2000);
+
+        }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                SendMailTest.SendEmail(e.Message.Trim(), e.StackTrace);
+            }
+            finally
+            {
+                driver.Quit();
+
+            }
+
+        }
+    }
+}
---
+++ Email/SendMailTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace SeleniumBasicProgram.Email
+{
+    public class SendMailTest
+    {
+        public static void SendEmail(String Subject, String contentBody)
+        {
+            MailMessage mail = new MailMessage();
+            String fromEmail = "Aswaraj78@gmail.com";
+            String password = "123456qwe789";
+            String ToEmail = "amitswaraj450@gmail.com";
+            mail.From = new MailAddress(fromEmail);
+            mail.To.Add(ToEmail);
+            mail.Subject = Subject.Replace('\r', ' ').Replace('\n', ' ');
+            mail.Body = contentBody;
+            mail.Priority = MailPriority.High;
+            mail.IsBodyHtml = true;
+            mail.Attachments.Add(new Attachment(@"C:/Users/Kis/source/repos/SeleniumBasicProgram/SeleniumBasicProgram/Report/index.html"));
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(fromEmail, password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+
+    }
+    }
+
+           
+            
+
+           
+
+            
+
+
+        
+   
