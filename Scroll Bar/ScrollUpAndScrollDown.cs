47719dfe287d77a36f92774e52c07abb7693b511
amitswaraj61 <aswaraj78@gmail.com>
2020-07-28 18:04:36 +0530

    Added Automation script for Scroll bar in selenium

---
+++ Scroll Bar/ScrollUpAndScrollDown.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumBasicProgram.Scroll_Bar
+{
+    class ScrollUpAndScrollDown
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void ScrollDownAndUpTest()
+        {
+            driver.Url = "https://en.wikipedia.org/wiki/Main_Page"; //navigate to wikipedia page
+
+            driver.Manage().Window.Maximize();  //maxiize the screen size
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); //impliccit wait for 20 sec
+
+            IJavaScriptExecutor jse = driver as IJavaScriptExecutor; //creating javascripterExecuter for perform scrolldown and scrollup
+
+            jse.ExecuteScript("window.scrollBy(0,500);"); // scrolling down in webpage
+            Thread.Sleep(5000); //Weight for 5 secs
+       
+
+
+            jse.ExecuteScript("window.scrollBy(0,-500);");  //scrolling up in webpage
+            Thread.Sleep(5000);  //Weight for 5 secs
+
+
+            driver.Quit(); //quit th driver
+        }
+    }
+}
