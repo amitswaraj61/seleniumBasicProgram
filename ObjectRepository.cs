5b4f4803b37920bd1b950a9718dfa5ef6a5c1475
amitswaraj61 <aswaraj78@gmail.com>
2020-07-28 18:02:37 +0530

    Added Automation script for object repository in selenium

---
+++ ObjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Configuration;
+
+
+namespace SeleniumBasicProgram
+{
+    class ObjectRepository
+    {
+        IWebDriver driver = new ChromeDriver();
+
+
+
+        [Test]
+        public void ReadConfigurationTest()
+        {
+            driver.Url= ConfigurationManager.AppSettings["URL"];
+            driver.Manage().Window.Maximize();
+            driver.Quit();
+        }
+
+    }
+}
